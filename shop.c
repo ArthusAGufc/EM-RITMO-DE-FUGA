0e5bf7422f5fca4fd1ea9765f1d5ce64fffc1ca7
Gabriel Novais <84047213+GNovaisL@users.noreply.github.com>
2022-01-30 19:53:14 -0300

    Update shop.c

--- shop.c
+++ shop.c
@@ -1,1 +1,18 @@
+#include "raylib.h"
 #include "jogo.h"
+
+void Shop(){
+
+    Windows();
+
+    Music music = LoadMusicStream("Imagens/Music/Teste.wav");
+    while (!WindowShouldClose()){
+        SetMusicVolume(music, volume);
+        PlayMusicStream(music);
+
+
+        UpdateMusicStream(music);
+    }
+
+    UnloadMusicStream(music);
+}
