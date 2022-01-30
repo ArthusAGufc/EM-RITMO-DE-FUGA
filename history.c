414c003a1b94fa4c485939b289295581527fe726
Gabriel Novais <84047213+GNovaisL@users.noreply.github.com>
2022-01-30 19:50:59 -0300

    Update history.c

--- history.c
+++ history.c
@@ -1,1 +1,20 @@
+#include "raylib.h"
 #include "jogo.h"
+
+
+void History(){
+
+    Windows();
+
+    Music music = LoadMusicStream("Imagens/Music/Teste.wav");
+    
+    while (!WindowShouldClose()){
+        SetMusicVolume(music, volume);
+        PlayMusicStream(music);
+
+
+        UpdateMusicStream(music);
+    }
+
+    UnloadMusicStream(music);
+}
