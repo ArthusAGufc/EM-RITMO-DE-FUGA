e13fb5858647984385f119345d15519578ec2f8c
Periliat <87018862+Periliat@users.noreply.github.com>
2022-02-05 20:53:56 -0300

    Update shop.c

--- shop.c
+++ shop.c
@@ -3,18 +3,19 @@
 #include <stdlib.h>
 #include "jogo.h"
 
+
+
 void shop(){
 
     const int screenWidth=800, screenHeight=500;
     int PosY=395, PosX=17;
-    int pts=0; //variavel temporaria, mudar pelos pontos quando feitos.
     
     InitWindow(screenWidth,screenHeight, "Shop");
 
     arquivo = fopen("HighScore.txt","r");
     fscanf(arquivo,"%d",&highscore);
     fclose(arquivo);
-    
+
     //Imagem de fundo
     Texture2D tipeone = LoadTexture("Imagens/Shop/loja_tipo_1.png");
     Texture2D tipetwo = LoadTexture("Imagens/Shop/loja_tipo_2.png");
@@ -25,7 +26,7 @@ void shop(){
 
     //Loop da loja
     while(!WindowShouldClose()){
-        
+
         SetMusicVolume(music, volume);
         PlayMusicStream(music);
 
@@ -53,30 +54,27 @@ void shop(){
             CloseWindow();
             Menu();
         }else if(PosX==17 && IsKeyPressed(KEY_ENTER)){
-            /*
-                code
-                        */
-        }else if(PosX==282 && IsKeyPressed(KEY_ENTER)){
-            /*
-                code
-                        */
-        }else if(PosX==547 && IsKeyPressed(KEY_ENTER)){
-            /*
-                code
-                        */
+            selecao = 1;
+            Menu();
+        }else if( (PosX==282 && highscore>=2700) && IsKeyPressed(KEY_ENTER)){
+            selecao = 2;
+            Menu();
+        }else if( (PosX==547 && highscore>=5400) && IsKeyPressed(KEY_ENTER)){
+            selecao = 3;
+            Menu();
         }
 
         BeginDrawing();
             ClearBackground(BLACK);
 
-            if(pts>=5400){
+            if(highscore>=5400){
                 DrawTextureV(tipetree, (Vector2) {0,0},WHITE);
-            }else if(pts>=2700){
+            }else if(highscore>=2700){
                 DrawTextureV(tipetwo, (Vector2) {0,0},WHITE);
             }else{
                 DrawTextureV(tipeone, (Vector2) {0,0},WHITE);
             }
-            DrawText(TextFormat("HighScore: %d",highscore),10,450,28,GREEN);
+            DrawText(TextFormat("HighScore: %d",highscore),10,450,28,DARKGRAY);
 
             DrawRectangleRoundedLines((Rectangle){PosX, PosY, 235, 30},0.3,5,5,LIGHTGRAY);
         EndDrawing();
